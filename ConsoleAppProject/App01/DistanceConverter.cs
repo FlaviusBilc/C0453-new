62925e0619eec5735cf479530dac03177263b508
Flavius Bilc <100183762@student.burnley.ac.uk>
2024-02-09 09:33:51 +0000

    09:33 09/02/2024

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System;   
 
 namespace ConsoleAppProject.App01
 {
@@ -12,27 +12,39 @@ namespace ConsoleAppProject.App01
     {
 
         private double miles;
-
         private double feet;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Run()
-        (
-
-        )
-
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
         private void InputMiles()
-        (
-
-        )
-
+        {
+            Console.Write("Please enter the number of miles: ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
         private void CalculateFeet()
-        (
-
-        )
-
+        { 
+        
+        }
+        /// <summary>
+        /// 
+        /// </summary>
         private void OutputFeet()
-        (
-
-        )
+        { 
+        
+        }
     }
 }
